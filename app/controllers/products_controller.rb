8e87d9d0a50155049c46b9c44afc99654d76b6c9
Michael Anderson <michanders@msn.com>
2018-02-22 22:45:03 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,16 +3,6 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
-  def inventory
-    product = Product.find(params[:id])
-    render plain: product.inventory > 0 ? true : false
-  end
-
-  def description
-    product = Product.find(params[:id])
-    render plain: product.description
-  end
-
   def new
     @product = Product.new
   end
